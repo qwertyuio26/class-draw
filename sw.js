642f9d90da4b344ba9d72c893a3b8d71af8767bf
qwertyuio26 <710823233@qq.com>
2026-09-02 15:05:20 +0800

    内置三班名单离线版 v3：roster-data + SW注册 + 存储降级

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'classdraw-v2';
+const CACHE = 'classdraw-v3';
 const ASSETS = ['./','./index.html','./app.js','./style.css','./manifest.json','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
 self.addEventListener('install', e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
